8eeff288bf06f31c17952b1d0335ce64a09492e3
sagar <sagarc2580@gmail.com>
2023-12-10 07:05:47 +0530

    learnt how to create hello world in both html and javascript

---
+++ index.js
@@ -0,0 +1,41 @@
+// building hello world using javascript
+//!! 1st example
+// const heading1 = document.createElement("h1");
+// heading1.innerHTML = "hello world from javascript";
+
+// const root = document.getElementById("root");
+// root.appendChild(heading1);
+//!! 2nd example
+// const divy = document.createElement("div");
+// const h1 = document.createElement("h4");
+// h1.innerHTML = "Hey sagar";
+
+// divy.appendChild(h1);
+// document.body.appendChild(divy);
+
+//!! 3rd example
+// Create a paragraph (<p>) element with the text "Hello, world!" and append it to the body of the document.
+
+// const para = document.createElement("p");
+// const heading = document.createElement("h1");
+// heading.innerHTML = "exercise 1 niggah";
+
+// para.appendChild(heading);
+// document.body.appendChild(para);
+
+//!! 4th example
+// Create an ordered list (<ol>) with three list items (<li>) containing the text "Item 1," "Item 2," and "Item 3." Append the list to the body.
+
+// const ordered = document.createElement("ol");
+// for (let i = 1; i <= 3; i++) {
+//   const items = document.createElement("li");
+
+//   items.innerHTML = "Item" + i;
+//   ordered.appendChild(items);
+// }
+
+// document.body.appendChild(ordered);
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Building hello world using react
---
+++ notes.md
@@ -0,0 +1,12 @@
+1. What is emmet
+   => emmet basically helps us to write html and css faster , it is like a abbreviation engine for vs code.
+
+2) Browser do not know react code,so we have to get react into our project
+
+3) first way to add react is by cdn
+   cdn links is nothing but conetnt delivery network , it is a place where react library is hosted
+
+4) what is CDN ?
+   => it is a content delivery network and it s a network of disttibuted servers that work together to deliver web content like image , stylesheets etc . when comes to react , cdn is used to deliver the library/framework files to users.
+
+5)
