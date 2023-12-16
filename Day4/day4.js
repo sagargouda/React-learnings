fb78156cfbb9d2de7c961f3b5e023e3651bb3643
sagar <sagarc2580@gmail.com>
2023-12-16 05:57:04 +0530

    assignment-3

--- Day4/day4.js
+++ Day4/day4.js
@@ -1,13 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-//?? assignments
-const header = React.createElement("div", { id: "container" }, [
-  React.createElement("div", { id: "heading1" }, "heading-1"),
-  React.createElement("div", { id: "heading2" }, "heading-2"),
-  React.createElement("div", { id: "heading-3" }, "headin-3"),
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(header);
---
+++ Day4/notes.md
@@ -0,0 +1,6 @@
+also remember in scripts you have given
+"start": "parcel Day3/index.html",
+"build": "parcel build Day3/index.html"
+so it will always run day 3 , so chnage it to day 4
+
+2. when writing image make sure you give image address in src and tag is <img/>
